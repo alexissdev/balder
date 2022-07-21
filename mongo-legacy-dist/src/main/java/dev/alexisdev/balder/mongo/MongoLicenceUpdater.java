a4d4806549adde9dab1cefcf29cd7be32688302f
alexissdev <notcacha@gmail.com>
2022-07-21 10:09:42 -0300

    feat(mongo-legacy-dist): addded the MongoLicenceUpdater

---
+++ mongo-legacy-dist/src/main/java/dev/alexisdev/balder/mongo/MongoLicenceUpdater.java
@@ -0,0 +1,39 @@
+package dev.alexisdev.balder.mongo;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.ReplaceOptions;
+import dev.alexisdev.balder.api.licence.Licence;
+import dev.alexisdev.balder.api.updater.LicenceUpdater;
+import org.bson.Document;
+
+public class MongoLicenceUpdater
+        implements LicenceUpdater {
+
+    protected final MongoCollection<Document> mongoCollection;
+
+    protected MongoLicenceUpdater(
+            MongoCollection<Document> mongoCollection
+    ) {
+        this.mongoCollection = mongoCollection;
+    }
+
+    @Override
+    public void update(
+            Licence licence
+    ) {
+        Document document = new Document();
+        document.append(
+                "_id", licence.getId()
+        );
+        document.append(
+                "address", licence.getAddress()
+        );
+
+        mongoCollection.replaceOne(
+                Filters.eq("_id", licence.getId()),
+                document,
+                new ReplaceOptions().upsert(true)
+        );
+    }
+}
