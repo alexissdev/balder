23fdeec2f20731a8885adbc2872ca40d3ecb8c40
alexissdev <notcacha@gmail.com>
2022-07-21 09:46:09 -0300

    deps(http-dist): added the gson dependency

--- http-dist/build.gradle.kts
+++ http-dist/build.gradle.kts
@@ -4,4 +4,5 @@ repositories {
 dependencies {
     implementation(project(":api"))
     implementation("org.apache.httpcomponents:httpclient:4.5.7")
+    implementation("com.google.code.gson:gson:2.9.0")
 }
