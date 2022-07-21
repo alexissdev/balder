608dbd7ea17b6b0fef4dbb2694d706452093f160
alexissdev <notcacha@gmail.com>
2022-07-20 21:14:15 -0300

    feat(model): added the Model class

---
+++ api/src/main/java/dev/alexisdev/balder/api/model/Model.java
@@ -0,0 +1,11 @@
+package dev.alexisdev.balder.api.model;
+
+public interface Model {
+
+    /**
+     * Returns the id of the model.
+     *
+     * @return The id of the model.
+     */
+    String getId();
+}
